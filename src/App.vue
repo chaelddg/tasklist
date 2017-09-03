66901040ee1b50a9410702de0a182e7d302e508a
Chael Kales <chael.ddg@gmail.com>
2017-09-03 17:23:37 +0800

    initial structure with iview

---
+++ src/App.vue
@@ -0,0 +1,29 @@
+<template>
+  <div id="app">
+    <Menu mode="horizontal" :theme="theme1" active-name="1">
+      <MenuItem name="1">
+        <Icon type="ios-paper"></Icon>
+        Home
+      </MenuItem>
+      <MenuItem name="2">
+        <Icon type="ios-gear"></Icon>
+        Settings
+      </MenuItem>
+    </Menu>
+    <router-view></router-view>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'app',
+  data() {
+    return {
+      theme1: 'light',
+    };
+  },
+};
+</script>
+
+<style>
+</style>
---
+++ src/components/Home.vue
@@ -0,0 +1,11 @@
+<template id="home-template">
+  <div>
+    <h1>Home</h1>
+  </div>
+</template>
+
+<script>
+</script>
+
+<style scoped>
+</style>
