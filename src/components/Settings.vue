a361233a30bbcff264accff4d88a6f058e03d5bc
Chael Kales <chael.ddg@gmail.com>
2017-09-03 19:21:33 +0800

    added routing and iview tasklist

---
+++ src/components/Settings.vue
@@ -0,0 +1,19 @@
+<template id="settings-template">
+  <div class="content-main">
+    <div class="layout-content-main">
+      <h1>Settings</h1>
+    </div>
+  </div>
+</template>
+
+<script>
+</script>
+
+<style scoped>
+  .content-main {
+    margin: 15px;
+  }
+  .layout-content-main {
+    padding: 10px;
+  }
+</style>
